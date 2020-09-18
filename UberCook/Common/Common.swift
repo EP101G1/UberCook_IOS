b9a41b760b6ec6cf5d135e301b762f84a1cc6aca
超 <49914028@stust.edu.tw>
2020-09-18 17:11:36 +0800

    no message

--- UberCook/Common/Common.swift
+++ UberCook/Common/Common.swift
@@ -11,8 +11,8 @@ let merchantId = "huangx10a_LINEPAY"
 // 實機
 // let URL_SERVER = "http://192.168.0.101:8080/Spot_MySQL_Web/"
 // 模擬器
-let common_url = "http://192.168.196.164:8080/UberCook_Server/"
-//let common_url = "http://127.0.0.1:8080/UberCook_Server/"
+//let common_url = "http://192.168.196.164:8080/UberCook_Server/"
+let common_url = "http://127.0.0.1:8080/UberCook_Server/"
 
 func executeTask(_ url_server: URL, _ requestParam: [String: Any], completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void)  {
     // requestParam值為Any就必須使用JSONSerialization.data()，而非JSONEncoder.encode()
